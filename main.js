36c3b582d910b751b7cd4b8e3f15982c64409fa2
Hungbanh1 <hungbg.dev1@gmail.com>
2024-03-18 07:11:26 +0700

    ex2

--- main.js
+++ main.js
@@ -1,29 +1,40 @@
+// main.js
 const app = Vue.createApp({
-    'template': `    {{ !isHello ? name + age: "" }}
-    <!-- solution 1 -->
-    <!-- <button v-on:click="onChangeName">Change</button> -->
-    <!-- solution 2 short  -->
-    <button @click="onChangeName">Change</button>
-    <div class="modal" v-if="isModal">
-      <h1>Modal content</h1>
-    </div>`,
-    data(){
-        return {
-            message: 'Hello Vue!',
-            name: "hung",
-            age: "15",
-            isHello : false,
-            isModal:false,
-        }
+    data() {
+      return {
+        message: 'Hello Vue!',
+        x: 0,
+        y: 0,
+        products: [
+            {id: 1, name: 'Product 1',img:'./assets/vue1.png',price:100,isCart:false},
+            {id: 2, name: 'Product 2',img:'./assets/vue2.jfif',price:120,isCart:false},
+            {id: 3, name: 'Product 3',img:'./assets/vue3.jfif',price:300,isCart:true}
+        ]
+      }
     },
     methods: {
-        onChangeName(){
-            console.log(this)
-            this.name = "name has changed"
-            this.age = "age has changed"
-            this.isModal = !this.isModal
-        }
-    },
-})
+      onLogEvent(event,newX,newY) {
+        console.log("running event")
+        this.x =newX
+        this.y = newY
+      },
+      onMouseMoveEvent(event) {
+        console.log("running mouse move event ...", event);
+        this.x = event.offsetX
+        this.y = event.offsetY
+      },
+      onToggleCart(event,product){
+        product.isCart =!product.isCart
 
-app.mount('#app')
+      }
+    },
+    computed:{
+        productsComputed(){
+            // return "string";
+            return this.products.filter((product) => product.price < 130);
+        }
+    }
+  })
+  
+  app.mount('#app')
+  
